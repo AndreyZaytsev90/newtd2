fe69ad01aa8010e6966967e826e2519dbd0ec584
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-05-06 13:49:05 +0300

    Updated file

--- newtd/src/App.tsx
+++ newtd/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
     return (
         <div className="App">
-            <h1>Hello!</h1>
+            <Todolist/>
+            <Todolist/>
             <Todolist/>
         </div>
     );
 }
 
-
-const Todolist = () => <h1>Todolist</h1>
 
 export default App;
 
